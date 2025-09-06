fb59b9940cafeaf3067e9e539060b4b932f55b92
Jorge <jorgejch@gmail.com>
2025-09-06 16:39:37 -0700

    remove Kubernetes namespace resource definition

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-resource "kubernetes_namespace" "n8n" {
-  metadata {
-    name = var.namespace_name
-    labels = {
-      name                                 = var.namespace_name
-      "pod-security.kubernetes.io/enforce" = "baseline"
-      "pod-security.kubernetes.io/audit"   = "baseline"
-      "pod-security.kubernetes.io/warn"    = "baseline"
-    }
-  }
-}
-
 resource "kubernetes_deployment" "n8n" {
   metadata {
     name      = "n8n"
