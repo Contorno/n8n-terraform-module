23376124296de5942b0609913590c736e502b9fc
Jorge <jorgejch@gmail.com>
2025-09-06 18:58:29 -0700

    Update ingress configuration to use 'configuration-snippet' for webhook path restrictions

--- ingress.tf
+++ ingress.tf
@@ -34,7 +34,7 @@ resource "kubernetes_ingress_v1" "n8n_webhooks" {
       "cert-manager.io/cluster-issuer"                 = "letsencrypt-prod"
       "nginx.ingress.kubernetes.io/ssl-redirect"       = "true"
       "nginx.ingress.kubernetes.io/force-ssl-redirect" = "true"
-      "nginx.ingress.kubernetes.io/server-snippet" = <<-EOT
+      "nginx.ingress.kubernetes.io/configuration-snippet" = <<-EOT
         # Block everything except webhook paths
         location / {
           if ($request_uri !~ ^/webhook) {
